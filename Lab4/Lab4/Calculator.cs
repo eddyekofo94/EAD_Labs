ec7f6febd8fbb31a9617b755d34f289b200d4737
Eddy Ekofo <contact@eddyekofo.com>
2017-10-05 11:53:34 +0100

    Lab4: finish the Calculator class

---
+++ Lab4/Lab4/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lab4
+{
+    public class Calculator
+    {
+        public float NumberToBeDevided { get; set; }
+        private float _numberThatDevides;
+
+        public Calculator()
+        {
+            
+        }
+        
+        public float NumberThatDevides
+        {
+            get => _numberThatDevides;
+            set
+            {
+            if (_numberThatDevides == 0)
+            {
+                Console.WriteLine("Can't devide by 0");
+                throw new ArgumentException();
+            }
+
+            return NumberToBeDevided = numberIn;
+                
+            }
+        }
+        
+    }
+}
