602cb69a0e6b52d4f33ac817c04c9bdc56f692f9
Eddy Ekofo <contact@eddyekofo.com>
2017-10-05 11:30:21 +0100

    Empty Lab4

---
+++ Lab4/Lab4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab4
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
