158bd2c0b416dfa97c0dadf56d9ee0b58bbcc2bc
Eddy Ekofo <contact@eddyekofo.com>
2017-10-07 13:37:06 +0100

    Working on Lab4

--- Lab4/Lab4/Program.cs
+++ Lab4/Lab4/Program.cs
@@ -7,6 +7,16 @@ namespace Lab4
     {
         public static void Main(string[] args)
         {
+            double dividend, divisor;
+            string dividendIn, divisorIn;
+            Console.WriteLine("Please enter the dividend");
+            dividendIn = Console.ReadLine();
+            
+            if (Double.Parse(dividendIn, out dividend))
+            {
+                
+            }
+            
             try
             {
                 Calculator.DivideFloatingPoints(2, 0);
@@ -16,7 +26,7 @@ namespace Lab4
             {
                 Console.WriteLine(e);
             }
-                Calculator.DivideFloatingPoints(4.5, 2);
+                Calculator.DivideFloatingPoints(4, 2);
             
         }
     }
